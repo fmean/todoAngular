8875cc16804812135b9c8021ed56d81d9def0da0
Amin <amin.fazilogly@gmail.com>
2020-06-13 21:28:33 +0200

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todoArray=[];
+  addTodo(value) {
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+  title = 'todo-app';
+  
+  /*delete item*/
+  deleteItem(todo) {
+    for(let i=0; i <= this.todoArray.length; i++){
+      if(todo==this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+  }
+
+  // submit form
+  todoSubmit(value:any){
+     console.error(value)
+  }
+}
